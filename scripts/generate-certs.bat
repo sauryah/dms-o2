b45f5eedad2742c22e758924f40bb8070a16fb83
sahil <sauryahjoshi@gmail.com>
2026-07-17 17:21:00 +0530

    Fix generate-certs.bat to copy rootCA and create DER format

--- scripts/generate-certs.bat
+++ scripts/generate-certs.bat
@@ -21,6 +21,18 @@ echo Generating TLS certificates...
 call mkcert -install 2>nul
 call mkcert -cert-file "%CERTS_DIR%\cert.pem" -key-file "%CERTS_DIR%\key.pem" localhost 127.0.0.1 %LAN_IP% ::1
 
+echo Copying root CA...
+for /f "tokens=*" %%i in ('mkcert -CAROOT') do set CAROOT=%%i
+if exist "%CAROOT%\rootCA.pem" (
+    copy "%CAROOT%\rootCA.pem" "%CERTS_DIR%\rootCA.pem" /Y >nul
+    certutil -encode "%CERTS_DIR%\rootCA.pem" "%CERTS_DIR%\rootCA.cer" >nul
+    certutil -decode "%CERTS_DIR%\rootCA.cer" "%CERTS_DIR%\rootCA.cer" >nul
+    echo   rootCA.pem - root CA (PEM format)
+    echo   rootCA.cer - root CA (DER format, for Windows import)
+) else (
+    echo   WARNING: Could not find rootCA.pem in mkcert CA store
+)
+
 echo.
 echo Certificates generated in %CERTS_DIR%\
 echo   cert.pem  - server certificate (valid for %LAN_IP%)
@@ -29,7 +41,7 @@ echo.
 echo Access your app at: https://%LAN_IP%
 echo.
 echo To access from another computer:
-echo   1. Copy %CERTS_DIR%\rootCA.pem to the other computer
-echo   2. Convert to .cer: openssl x509 -in rootCA.pem -outform DER -out rootCA.cer
-echo   3. Install rootCA.cer in Trusted Root Certification Authorities
+echo   1. Copy rootCA.cer to the other computer
+echo   2. Run: certutil -addstore -f "Root" rootCA.cer
+echo   3. Restart browser and visit https://%LAN_IP%
 pause
