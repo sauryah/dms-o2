6899c9932ddf28d3519d9dcf9ef7caf2f40757c9
sahil <sauryahjoshi@gmail.com>
2026-09-23 13:37:53 +0530

    feat(frontend): add /metallurgy-workbench route with lazy loading

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -23,6 +23,7 @@ const SettingsPage = lazyWithRetry(() => import('./pages/SettingsPage').then(m =
 const ToolsPage = lazyWithRetry(() => import('./pages/ToolsPage').then(m => ({ default: m.ToolsPage })))
 const WireDrawingCalculatorPage = lazyWithRetry(() => import('./pages/WireDrawingCalculatorPage').then(m => ({ default: m.WireDrawingCalculatorPage })))
 const DieSeriesGeneratorPage = lazyWithRetry(() => import('./pages/DieSeriesGeneratorPage').then(m => ({ default: m.DieSeriesGeneratorPage })))
+const MetallurgyWorkbenchPage = lazyWithRetry(() => import('./features/metallurgy-workbench/pages/MetallurgyWorkbenchPage').then(m => ({ default: m.MetallurgyWorkbenchPage })))
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -223,6 +224,13 @@ function AppContent() {
               <ErrorBoundary>
                 <ProtectedRoute requireToolAuth toolId="die-series-generator">
                   <DieSeriesGeneratorPage />
+                </ProtectedRoute>
+              </ErrorBoundary>
+            } />
+            <Route path="/metallurgy-workbench" element={
+              <ErrorBoundary>
+                <ProtectedRoute requireToolAuth toolId="metallurgy-workbench">
+                  <MetallurgyWorkbenchPage />
                 </ProtectedRoute>
               </ErrorBoundary>
             } />
