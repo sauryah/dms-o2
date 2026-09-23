6740d3c05e299fb4085f75e719f18720965e4648
sahil <sauryahjoshi@gmail.com>
2026-09-23 13:38:37 +0530

    feat(edge-gateway): declare Modbus RTU connection handling in modbus_client.h

--- services/edge-gateway/src/modbus_client.h
+++ services/edge-gateway/src/modbus_client.h
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <modbus.h>
+#include "config.h"
 
 #define MODBUS_REG_SPEED       0
 #define MODBUS_REG_TENSION     1
@@ -22,13 +23,20 @@ typedef struct {
 
 typedef struct {
     modbus_t *ctx;
+    char mode[16];
     char host[128];
     int port;
+    char serial_device[128];
+    int baud_rate;
+    char parity;
+    int data_bits;
+    int stop_bits;
     int slave_id;
     bool is_connected;
 } modbus_client_t;
 
 modbus_client_t* modbus_client_create(const char *host, int port, int slave_id);
+modbus_client_t* modbus_client_create_from_config(const gateway_config_t *cfg);
 bool modbus_client_connect(modbus_client_t *client);
 bool modbus_client_read_telemetry(modbus_client_t *client, machine_telemetry_t *out);
 void modbus_client_close(modbus_client_t *client);
